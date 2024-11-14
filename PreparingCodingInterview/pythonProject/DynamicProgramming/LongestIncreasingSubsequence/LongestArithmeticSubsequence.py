03bae915b2e2c430cdad72e04761a9155e6f8fd3
tung leduc <chopperman259@gmail.com>
2024-11-14 22:07:17 +0700

    solve daily problems: dynamic programming: LIS

--- PreparingCodingInterview/pythonProject/DynamicProgramming/LongestIncreasingSubsequence/LongestArithmeticSubsequence.py
+++ PreparingCodingInterview/pythonProject/DynamicProgramming/LongestIncreasingSubsequence/LongestArithmeticSubsequence.py
@@ -6,4 +6,28 @@ class LongestArithmeticSubsequenceSolution:
         self.nums: List[int] = nums
 
     def solve(self) -> int:
-        return 0
+        max_val = max(self.nums)
+        dp = [[0] * (max_val + 1) for _ in range(max_val + 1)]
+        max_dp = 0
+        for c in self.nums:
+            for prev in range(max_val + 1):
+                if prev == c:
+                    dp[c][c] += 1
+                    continue
+                step = c - prev
+                before = prev - step
+                if dp[prev][prev] != 0:
+                    dp[c][prev] = max(2, dp[c][prev])
+
+                if before < 0 or before > max_val:
+                    continue
+                dp[c][prev] = max(dp[prev][before] + 1, dp[c][prev])
+                max_dp = max(max_dp, dp[c][prev])
+
+        return max_dp
+
+
+nums1 = [0,500]
+
+sln = LongestArithmeticSubsequenceSolution(nums1)
+print(sln.solve())
