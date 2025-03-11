16a6faab816abac453d67e297a97fa317c3ee37d
tung leduc <chopperman259@gmail.com>
2025-03-12 04:43:40 +0700

    solve problem follow by topic: monotonic stack

--- PreparingCodingInterview/pythonProject/Stack/MonotonicStack/RemoveDuplicateLetters.py
+++ PreparingCodingInterview/pythonProject/Stack/MonotonicStack/RemoveDuplicateLetters.py
@@ -1,43 +1,10 @@
-from typing import List, Deque as Stack, DefaultDict as Table
-from collections import deque as stack, Counter, defaultdict
+from typing import List, Deque as Stack
+from collections import deque as stack, Counter
+
 
 class RemoveDuplicateLettersSolution:
     def __init__(self, s: str):
         self.s: str = s
-
-    def greedy_solve(self) -> str:
-
-        map_tb: Table[str, List[int]] = defaultdict(list)
-
-        for idx, c in enumerate(self.s):
-            map_tb[c].append(idx)
-
-        result = ''
-        threshold_idx = -1
-        choose_idx: List[int] = []
-        for i in range(26):
-
-            c = chr(i + ord('a'))
-
-            if not map_tb[c]:
-                continue
-            c_idx: int = map_tb[c][0]
-
-            for idx in map_tb[c]:
-                c_idx = idx
-                if idx > threshold_idx:
-                    break
-            choose_idx.append(c_idx)
-            threshold_idx = max(c_idx, threshold_idx)
-
-        result= ''
-
-        for idx in sorted(choose_idx):
-            result += self.s[idx]
-        return result
-
-
-
 
     def solve(self) -> str:
 
@@ -50,26 +17,22 @@ class RemoveDuplicateLettersSolution:
 
         result = ''
         mark: List[bool] = [False] * 26
-
         for idx, c in enumerate(nums):
+            freq[c] -= 1
             if mark[c]:
                 continue
-            while st and nums[st[-1]] >= c:
+            while st and nums[st[-1]] > c and freq[nums[st[-1]]] > 0:
                 ix = st.pop()
-                freq[nums[ix]] -= 1
+                mark[nums[ix]] = False
             st.append(idx)
+            mark[c] = True
 
-            if freq[nums[idx]] > 1:
-                continue
-
-            while st:
-                ix = st.popleft()
-                result += self.s[ix]
-                mark[nums[ix]] = True
+        for idx in st:
+            result += self.s[idx]
 
         return result
 
 
 s1 = "cbacdcbc"
 
-print(RemoveDuplicateLettersSolution(s1).greedy_solve())
+print(RemoveDuplicateLettersSolution(s1).solve())
